c960427c0a436958834b53cb16cda23066154ed2
alexanderleecii <43746763+alexanderleecii@users.noreply.github.com>
2018-11-28 22:42:03 +0100

    Update case.swift

--- case.swift
+++ case.swift
@@ -1,6 +1,18 @@
-Protocol Case : {
-	CreerCase : -> Case
-	EtatCase : Case -> Bool
-	//vide ou pleine
-	ModifierCase : Case -> Case
+//Case représente une case du ChampBataille (ou Royaume ?) sur laquelle pourra ou non être présente une carte
+protocol Case : {
+    //init : -> Case
+    //Création d'une Case initialisée avec un état (vide)
+    init()
+    
+    
+    //etatCase : Case -> Bool
+    //retourne vrai si la Case est occupée par une Carte, faux sinon
+    //Post: init() => EtatCase() == Faux
+    func etatCase() -> Bool
+    
+    //modifierCase : Case x Bool -> Case
+    //Change l'état de la Case par le booléen passé en paramètre
+    //Post: modifierCase(newEtat:Bool) => etatCase() == newEtat
+    @discardableResult
+    mutating func modifierCase(newEtat : Bool) -> Self
 }
