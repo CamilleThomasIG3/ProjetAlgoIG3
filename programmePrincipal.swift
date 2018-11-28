37bf914339656967ad03af6d79ddbddd4c5b168f
THOMAS Camille p1517668 <camille.thomas2@etu.univ-lyon1.fr>
2018-11-28 23:13:33 +0100

    debut programmePrincipale

--- programmePrincipal.swift
+++ programmePrincipal.swift
@@ -1,3 +1,33 @@
 func main(){
 
+  debutPartie()
+  var numJoueur : String = "J1"
+  var carte : Carte
+  var fin : Bool = finPartie()
+
+  while !fin
+  {
+    //préposition
+    print("Joueur "+numJoueur+", vos cartes sont remises en position défense et vous piochez une carte :\n")
+    carte=premierPioche()
+    ajouterMain(carte)
+    piocher()
+    print("carte "+nom(carte)+" de défense "+defense(carte)+" et d'attaque "+attaque(carte)+"\n")
+
+    //
+    numJoueur="J2"
+    fin=true
+ }
 }
+
+func debutPartie()
+{
+  //initialisation plateau
+}
+
+func finPartie()->Bool
+{
+  return false
+}
+
+main()
