1e145c69b20948fe16809070c4d76333c67f7613
alexanderleecii <43746763+alexanderleecii@users.noreply.github.com>
2018-11-28 22:46:49 +0100

    Update champsBataille.swift

--- champsBataille.swift
+++ champsBataille.swift
@@ -1,7 +1,7 @@
 //ChampBataille représente, pour un joueur, son côté du champ de bataille. Il est composé de 6 positions
 protocol ChampBataille  : Sequence {
     associatedtype IteratorCB : IteratorProtocol
-    where IteratorCB.Element == Case
+        where IteratorCB.Element == Case
     
     //init : -> Champ
     //Création d'un ChampBataille composé de 6 Cases
