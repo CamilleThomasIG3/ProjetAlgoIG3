60db4e5d7cf9aff601026977929684c094a5eb77
alexanderleecii <43746763+alexanderleecii@users.noreply.github.com>
2018-11-28 22:43:14 +0100

    Update champsBataille.swift

--- champsBataille.swift
+++ champsBataille.swift
@@ -1,5 +1,18 @@
-protocol ChampsBataille  : Sequence { //iterateur
-	Associatedtype IteratorCB : IteratorProtocol
-	CreerChamps : -> [Case](6)
-	ChampVide : ChampsBataille -> Booleen
+//ChampBataille représente, pour un joueur, son côté du champ de bataille. Il est composé de 6 positions
+protocol ChampBataille  : Sequence {
+    associatedtype IteratorCB : IteratorProtocol
+    where IteratorCB.Element == Case
+    
+    //init : -> Champ
+    //Création d'un ChampBataille composé de 6 Cases
+    init()
+    
+    //ChampVide : ChampBataille -> Bool
+    //retourne un vrai si le ChampBataille est vide, faux sinon
+    //Post : init() => champVide() == Vrai
+    func champVide() -> Bool
+    
+    //makeIterator : ChampBataille -> IteratorCB
+    //Crée un itérateur sur la collection pour la parcourir dans l'ordre des Cases qui la composent
+    func makeIterator() -> IteratorCB
 }
