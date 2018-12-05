9b7d27c65e21df7eb767425a6674ba34c1e2a86c
alexanderleecii <43746763+alexanderleecii@users.noreply.github.com>
2018-12-05 13:02:10 +0100

    Update main.swift (chgt préconditions)

--- main.swift
+++ main.swift
@@ -7,11 +7,12 @@ protocol Main : Sequence{
 
 	//init : -> Main
 	//créer une main avec une seule carte : le roi du joueur (les joueurs ont des rois différents)
+	//précondition: la carte doit être un Roi (nom "Roi")
 	init(roi : Carte)
 
 	//AjouterMain : Main x Carte -> Main
 	//ajoute la carte passée en paramétre à la main
-	//postcondition : nbreCartesMain(ajouterMain(carte))=nbreCartesMain()+1
+	//postcondition : ajouterMain(carte) => nbreCartesMain()=nbreCartesMain()+1
 	@discardableResult
 	mutating func ajouterMain(carte : Carte)->Self
 
@@ -19,26 +20,27 @@ protocol Main : Sequence{
 	//Indique si la carte appartient à la main
 	//resultat : retourne vrai si la carte appartient à la main et faux si
 	//elle n'appartient pas à la main ou si la main est vide
-	//postcondition : si appartientMain(carte)==True => nbreCartesMain()>0
+	//postcondition : ajouterMain(carte) => appartientMain(carte)==True => nbreCartesMain()>0
 	func appartientMain(carte : Carte)->Bool
 
 	//EnleverMain : Main x Carte -> Main
 	//supprime la carte passée en paramétre de la main
 	//precondition : appartientMain(carte)==True
-	//postcondition : nbreCartesMain(enleverMain(carte))=nbreCartesMain()-1
+	//postcondition : enleverMain(carte) => nbreCartesMain()=nbreCartesMain()-1
+	//postcondition : enleverMain(carte) => appartientMain(carte) == false
 	@discardableResult
 	mutating func enleverMain(carte : Carte)->Self
 
 	//getCarte : StringxIntxIntxMain -> Carte
 	//retourne la carte avec le nom, la defense et attaque passés en parametre
 	//données : le nom de la carte, les points de defense, les points d'attaque
-	func getCarte(nom : String, defense : Int, attaque : Int?) -> Carte
+	func getCarte(nom : String, defense : Int, attaque : Int?) -> Carte //pq attaque est Int? sert à quoi ? il faut passer l'emplacement en parametre et verifier qu'il soit egal à la main
 
 	//MainVide : Main -> Booleen
 	//Indique si la main est vide ou non
 	//resultat : Retourne vrai si la main est vide et faux sinon
-	//postcondition : si mainVide()==True => nbreCartesMain()=0
-	//postcondition : si mainVide()==False => nbreCartesMain()>0
+	//postcondition : nbreCartesMain()=0 => mainVide()==True
+	//postcondition : nbreCartesMain()>0 => mainVide()==False 
 	func mainVide()->Bool
 
 	//NbreCartesMain : Main -> Int
