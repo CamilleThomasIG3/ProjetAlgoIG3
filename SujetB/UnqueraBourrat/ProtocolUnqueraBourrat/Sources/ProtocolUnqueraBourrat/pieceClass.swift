daf5d778858c135ad1686854969a639deed22923
CamilleThomasIG3 <camille.thomas@etu.umontpellier.fr>
2018-12-31 17:45:48 +0100

    Protocoles compilent

--- SujetB/UnqueraBourrat/ProtocolUnqueraBourrat/Sources/ProtocolUnqueraBourrat/pieceClass.swift
+++ SujetB/UnqueraBourrat/ProtocolUnqueraBourrat/Sources/ProtocolUnqueraBourrat/pieceClass.swift
@@ -4,50 +4,50 @@ class Piece : PieceProtocol{
     private var position : Position
     private var joueur : Int
     private var reserveBool : Bool = false
-    
+
     required init(numero:  Int, nom: String, position: Position, joueur: Int){
         self.numero = numero
         self.nom = nom
         self.position = position
         self.joueur = joueur
     }
-    
+
     func getPosition() -> Position{
         return self.position
     }
-    
+
     func getJoueur() -> Int{
         return self.joueur
     }
-    
+
     func estEnReserve() -> Bool{
         return self.reserveBool
     }
-    
+
     func getNom() -> String{
         return self.nom
     }
-    
+
     func getNumero() -> Int{
         return self.numero
     }
-    
+
     func deplacementPossible(position : Position) -> Bool{
         return !position.getB()
     }
-    
+
     @discardableResult
     func changerNom(nom : String)->Self{
         self.nom = nom
 	return self
     }
-    
+
     @discardableResult
     func changerJoueur(joueur : Int)->Self{
         self.joueur = joueur
 	return self
     }
-    
+
     @discardableResult
     func parachuter(position : Position)->Self{
         if self.estEnReserve(){
@@ -59,16 +59,16 @@ class Piece : PieceProtocol{
         }
 	return self
     }
-    
+
     @discardableResult
     func deplacer(position : Position)->Self{
         self.position = position
 	return self
     }
-    
+
     @discardableResult
     func seFaireCapturer()->Self{
-        var reserve : Position = Position(x : -1, y: -1, b: true)
+        let reserve : Position = Position(x : -1, y: -1, b: true)
         self.position = reserve
         if self.joueur == 1{
             self.joueur = 2
@@ -78,13 +78,13 @@ class Piece : PieceProtocol{
         }
 	return self
     }
-    
+
     @discardableResult
     func evoluer() -> Self{
         self.nom = "kodama samourai"
         return self
     }
-    
+
     @discardableResult
     func diminuer() -> Self{
         self.nom = "kodama"
